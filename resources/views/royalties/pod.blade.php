6c3de31db6c9fbe94302b748c4b8bf10fd6d594f
Elink mAster <arieldumalag@readersmagnet.com>
2022-10-25 22:23:26 +0800

    remove button

--- resources/views/royalties/pod.blade.php
+++ resources/views/royalties/pod.blade.php
@@ -28,30 +28,11 @@
                 </form>
                 
                 <div class="ms-auto">
-                    <a href="{{ route('pod.import-page') }}" class="btn btn-outline-primary">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                            class="bi bi-download" viewBox="0 0 16 16">
-                            <path
-                                d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z" />
-                            <path
-                                d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z" />
-                        </svg>
-                        Bulk Import
-                    </a>
-                    {{-- <a href="{{route('pod.create')}}" class="btn btn-outline-success">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-plus" viewBox="0 0 16 16">
-                        <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H1s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C9.516 10.68 8.289 10 6 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
-                        <path fill-rule="evenodd" d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z"/>
-                      </svg>
-                    Add Transaction
-                </a> --}}
-                <a href="{{ route('pod.clear') }}"
-                                            onclick="return confirm('Are you sure you want to DELETE ALL POD?')"
-                                            class="btn btn-danger" width="16" height="16"> Clear All</a>
+                    
                 </div>
             </div>
             <div class="bg-light p-2 shadow rounded">
-                <h5 class="text-center my-3">Pod Royalt</h5>
+                <h5 class="text-center my-3">Pod Royalty</h5>
                 <table class="table table-bordered table-hover mt-2">
                     <thead>
                         <tr class="text-center">
