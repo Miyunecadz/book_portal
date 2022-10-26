736fd13e52ba034a7662574ed89859c2a4066d7b
Elink mAster <arieldumalag@readersmagnet.com>
2022-10-27 07:39:38 +0800

    adjusted field

--- resources/views/prev.blade.php
+++ resources/views/prev.blade.php
@@ -57,7 +57,7 @@
                             <td style="border: 1px solid; width:70px; text-align:center;">{{$pod['quantity']}}</td>
                             <td style="border: 1px solid; width:70px; text-align:center;">{{$pod['price']}}</td>
                             <td style="border: 1px solid; width:70px; text-align:center;">{{$pod['revenue']}}</td>
-                            <td style="border: 1px solid; width:70px; text-align:center;">{{$pod['royalty']}}</td>
+                            <td style="border: 1px solid; width:70px; text-align:center;">{{substr($pod['royalty'],0,-1)}}</td>
                         </tr>
                     @endif
                 @endforeach
