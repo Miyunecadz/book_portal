f644d91a11cd9bf83d047c6c4d3958ebc316fa21
Elink mAster <arieldumalag@readersmagnet.com>
2022-11-01 03:02:58 +0800

    adjusted font ,money in words , date time

--- resources/views/report/pdf.blade.php
+++ resources/views/report/pdf.blade.php
@@ -30,11 +30,11 @@
     <div class="details" style="margin-top: 30px;">
        {{$currentDate}} EST
         <br>
-        <span>Statement Period: <b>{{App\Helpers\MonthHelper::getStringMonth($fromMonth)}} {{$fromYear}}</b> to <b>{{App\Helpers\MonthHelper::getStringMonth($toMonth)}} {{$toYear}}</b></span>
+        <span style="font-family:Verdana,  sans-serif;">Statement Period: <b>{{App\Helpers\MonthHelper::getStringMonth($fromMonth)}} {{$fromYear}}</b> to <b>{{App\Helpers\MonthHelper::getStringMonth($toMonth)}} {{$toYear}}</b></span>
         <br>
-        <h6 class="mt-4" style="font:Arial; font-size: 15px;"><b>{{$author->getFullName()}}</b></h6>
+        <h6 class="mt-4" style="font-family:Verdana,  sans-serif; font-size: 15px;"><b>{{$author->getFullName()}}</b></h6>
     </div>
-    <div class="details" style="margin-top: 30px; font-family: Roboto;" >
+    <div class="details" style="margin-top: 30px; " >
       Dear {{$author->firstname}},<br>
       @if(!empty($totalPods['royalty']))
       Enclosed is the royalty payment amounting to <span>${{substr($totalPods['royalty'],0,-1)}}</span> ({{App\Helpers\NumberFormatterHelper::numtowords(substr($totalPods['royalty'],0,-1)) }}).
@@ -49,9 +49,9 @@
        <br>
        <br>
         @if(count($pods) > 0)
-        <table style="width:100%;font-size: 14px; font-family:Roboto;">
+        <table style="padding-right: 20px; width:80%; font-size: 14px; font-family:Roboto;">
             <thead style=" background-color:#336EFF ;border: 1px solid;font-size: 12px; color:#EBD5D1;">
-                <tr style="font-family:Roboto; text-align:center; ">
+                <tr style="font-family:Verdana, Helvetica, sans-serif; text-align:center; ">
                     <th style=" border: 1px solid;"><span>Book Title</span></th>
                     <th style=" border: 1px solid;">Format</th>
                     <th style=" border: 1px solid;">Month</th>
@@ -65,32 +65,32 @@
             <tbody style="" >
                 @foreach ($pods as $pod)
                     @if(App\Helpers\UtilityHelper::hasTotalString($pod))
-                        <tr style="font-family:Roboto;">
-                            <td colspan="4"style="font-family:Roboto; border:1px solid; width:90px; background-color:#84A2FF ;"><b style="font-family:">{{$pod['title']}}</b></td>
-                            <td style="border: 1px solid; width:70px; background-color:#84A8FF ; text-align:center;"><b>{{$pod['quantity']}}</b></td>
-                            <td style="border: 1px solid; width:70px; background-color:#84A8FF ;text-align:center;"><b>${{$pod['price']}}</b></td>
-                            <td style="border: 1px solid; width:70px; background-color:#84A8FF ;text-align:center;"><b>${{$pod['revenue']}}</b></td>
-                            <td style="border: 1px solid; width:70px; background-color:#84A8FF ;text-align:center;"><b>${{substr($pod['royalty'],0,-1)}}</b></td>
+                        <tr style="">
+                            <td colspan="4"style="font-family:Verdana, Helvetica, sans-serif; border:1px solid; width:90px; background-color:#84A2FF ;"><b>{{$pod['title']}}</b></td>
+                            <td style="font-family:Verdana, Helvetica, sans-serif; border: 1px solid; width:70px; background-color:#84A8FF ; text-align:center;"><b>{{$pod['quantity']}}</b></td>
+                            <td style="font-family:Verdana, Helvetica, sans-serif; border: 1px solid; width:70px; background-color:#84A8FF ;text-align:center;"><b>${{$pod['price']}}</b></td>
+                            <td style="font-family:Verdana, Helvetica, sans-serif; border: 1px solid; width:70px; background-color:#84A8FF ;text-align:center;"><b>${{$pod['revenue']}}</b></td>
+                            <td style="font-family:Verdana, Helvetica, sans-serif; border: 1px solid; width:70px; background-color:#84A8FF ;text-align:center;"><b>${{substr($pod['royalty'],0,-1)}}</b></td>
                         </tr>
                     @else
                         <tr>
-                            <td style="border: 1px solid; width:230px;" >{{$pod['title']}}</td>
-                            <td style="border: 1px solid; width:90px; text-align:center;">{{$pod['format']}}</td>
+                            <td style="border: 1px solid; width:200px;" >{{$pod['title']}}</td>
+                            <td style="border: 1px solid; width:80px; text-align:center;">{{$pod['format']}}</td>
                             <td style="border: 1px solid; width:50px; text-align:center;">{{App\Helpers\MonthHelper::getStringMonth($pod['month'])}}</td>
-                            <td style="border: 1px solid; width:50px; text-align:center;">{{$pod['year']}}</td>
-                            <td style="border: 1px solid; width:70px; text-align:center;">{{$pod['quantity']}}</td>
-                            <td style="border: 1px solid; width:70px; text-align:center;">{{$pod['price']}}</td>
-                            <td style="border: 1px solid; width:70px; text-align:center;">{{$pod['revenue']}}</td>
-                            <td style="border: 1px solid; width:70px; text-align:center;">{{substr($pod['royalty'],0,-1)}}</td>
+                            <td style="border: 1px solid; width:45px; text-align:center;">{{$pod['year']}}</td>
+                            <td style="border: 1px solid; width:60px; text-align:center;">{{$pod['quantity']}}</td>
+                            <td style="border: 1px solid; width:60px; text-align:center;">{{$pod['price']}}</td>
+                            <td style="border: 1px solid; width:60px; text-align:center;">{{$pod['revenue']}}</td>
+                            <td style="border: 1px solid; width:60px; text-align:center;">{{substr($pod['royalty'],0,-1)}}</td>
                         </tr>
                     @endif
                 @endforeach
                 <tr style="font-family:Calibri; width:70px;background-color:#336EFF; color: #FFFFFF; ;">
-                <td colspan="4" style="border: 1px solid; width:90px; font-family:Roboto;  background-color:#336EFF;color: #FFFFFF;"><b>{{$totalPods['title']}}</b></td>
-                    <td style="text-align:center "><b>{{$totalPods['quantity']}}</b></td>
-                    <td style=" text-align:center"><b>${{$totalPods['price']}}</b></td> 
-                    <td style=" text-align:center"><b>${{$totalPods['revenue']}}</b></td>
-                    <td style=" text-align:center"><b>${{substr($totalPods['royalty'],0,-1)}}</b></td>
+                <td colspan="4" style="border: 1px solid; width:90px; font-family:Verdana, Helvetica, sans-serif;  background-color:#336EFF;color: #FFFFFF;"><b>{{$totalPods['title']}}</b></td>
+                    <td style="font-family:Verdana, Helvetica, sans-serif; text-align:center "><b>{{$totalPods['quantity']}}</b></td>
+                    <td style="font-family:Verdana, Helvetica, sans-serif; text-align:center"><b>${{$totalPods['price']}}</b></td> 
+                    <td style="font-family:Verdana, Helvetica, sans-serif; text-align:center"><b>${{$totalPods['revenue']}}</b></td>
+                    <td style="font-family:Verdana, Helvetica, sans-serif; text-align:center"><b>${{substr($totalPods['royalty'],0,-1)}}</b></td>
                 </tr>
             </tbody>
         </table>
@@ -100,7 +100,7 @@
     <div class="transaction" style="margin-top: 10px;">
         <table style="width:100%;font-size: 14px;">
         <thead style=" background-color:#336EFF ;border: 1px solid;font-size: 12px; color:#EBD5D1;">
-                <tr style="text-align:center; ">
+                <tr style="font-family:Verdana, Helvetica, sans-serif; text-align:center; ">
                 <th style="border: 1px solid;">eBook</th>
                     <th style="border: 1px solid;">Month</th>
                     <th style="border: 1px solid;">Year</th>
@@ -115,33 +115,33 @@
                 @foreach ($ebooks as $ebook)
                     @if(App\Helpers\UtilityHelper::hasTotalString($ebook))
                     <tr>
-                        <td colspan="4"style="font-family:Roboto; border:1px solid; width:90px; background-color:#84A2FF ;"><b>{{$ebook['title']}}</b></td>
-                        <td style="border: 1px solid; width:70px; background-color:#84A8FF ; text-align:center;"><b></b></td>
-                        <td style="border: 1px solid; width:70px; background-color:#84A8FF ; text-align:center;"><b>{{$ebook['quantity']}}</b></td>
-                        <td style="border: 1px solid; width:70px; background-color:#84A8FF ; text-align:center;"><b>{{$ebook['price']}}</b></td>
-                        <td style="border: 1px solid; width:70px; background-color:#84A8FF ; text-align:center;"><b>{{$ebook['revenue']}}</b></td>
-                        <td style="border: 1px solid; width:70px; background-color:#84A8FF ; text-align:center;"><b>{{$ebook['royalty']}}</b></td>
+                        <td colspan="4"style="font-family:Verdana, Helvetica, sans-serif; border:1px solid; width:80px; background-color:#84A2FF ;"><b>{{$ebook['title']}}</b></td>
+
+                        <td style="font-family:Verdana, Helvetica, sans-serif;border: 1px solid; width:70px; background-color:#84A8FF ; text-align:center;"><b>{{$ebook['quantity']}}</b></td>
+                        <td style="font-family:Verdana, Helvetica, sans-serif;border: 1px solid; width:70px; background-color:#84A8FF ; text-align:center;"><b>{{$ebook['price']}}</b></td>
+                        <td style="font-family:Verdana, Helvetica, sans-serif;border: 1px solid; width:70px; background-color:#84A8FF ; text-align:center;"><b>{{$ebook['revenue']}}</b></td>
+                        <td style="font-family:Verdana, Helvetica, sans-serif;border: 1px solid; width:70px; background-color:#84A8FF ; text-align:center;"><b>{{$ebook['royalty']}}</b></td>
                     </tr>
                     @else
                     <tr>
                         <td style="border: 1px solid; width:230px;" >{{$ebook['title']}}</td>
-                        <td style="border: 1px solid; width:90px; text-align:center;">{{App\Helpers\MonthHelper::getStringMonth($ebook['month'])}}</td>
-                        <td style="border: 1px solid; width:50px; text-align:center;">{{$ebook['year']}}</td>
-                        <td style="border: 1px solid; width:50px; text-align:center;">{{$ebook['trade']}}</td>
-                        <td style="border: 1px solid; width:50px; text-align:center;">{{$ebook['quantity']}}</td>
-                        <td style="border: 1px solid; width:70px; text-align:center;">{{$ebook['price']}}</td>
-                        <td style="border: 1px solid; width:70px; text-align:center;">{{$ebook['revenue']}}</td>
-                        <td style="border: 1px solid; width:70px; text-align:center;">{{$ebook['royalty']}}</td>
+                        <td style="border: 1px solid; width:75px; text-align:center;">{{App\Helpers\MonthHelper::getStringMonth($ebook['month'])}}</td>
+                        <td style="border: 1px solid; width:45px; text-align:center;">{{$ebook['year']}}</td>
+                        <td style="border: 1px solid; width:45px; text-align:center;">{{$ebook['trade']}}</td>
+                        <td style="border: 1px solid; width:45px; text-align:center;">{{$ebook['quantity']}}</td>
+                        <td style="border: 1px solid; width:60px; text-align:center;">{{$ebook['price']}}</td>
+                        <td style="border: 1px solid; width:60px; text-align:center;">{{$ebook['revenue']}}</td>
+                        <td style="border: 1px solid; width:60px; text-align:center;">{{$ebook['royalty']}}</td>
                     </tr>
                     @endif
                 @endforeach
                 <tr style="font-family:Calibri; width:70px;background-color:#336EFF; color: #FFFFFF;">
-                <td colspan="3" style="border: 1px solid; width:90px; "><b>{{$totalEbooks['title']}}</b></td>
-                    <td style="border: 1px solid; width:70px; text-align:center;"><b></b></td>
-                    <td style="border: 1px solid; width:70px; text-align:center;"><b>{{$totalEbooks['quantity']}}</b></td>
-                    <td style="border: 1px solid; width:70px; text-align:center;"><b>{{$totalEbooks['royalty']}}</b></td>
-                    <td style="border: 1px solid; width:70px; text-align:center;"><b>{{$totalEbooks['royalty']}}</b></td>
-                    <td style="border: 1px solid; width:70px; text-align:center;"><b>{{$totalEbooks['royalty']}}</b></td>
+                <td colspan="3" style="font-family:Verdana, Helvetica, sans-serif; border: 1px solid; width:90px; "><b>{{$totalEbooks['title']}}</b></td>
+                    <td style="font-family:Verdana, Helvetica, sans-serif;border: 1px solid; width:70px; text-align:center;"><b></b></td>
+                    <td style="font-family:Verdana, Helvetica, sans-serif;border: 1px solid; width:70px; text-align:center;"><b>{{$totalEbooks['quantity']}}</b></td>
+                    <td style="font-family:Verdana, Helvetica, sans-serif;border: 1px solid; width:70px; text-align:center;"><b>{{$totalEbooks['royalty']}}</b></td>
+                    <td style="font-family:Verdana, Helvetica, sans-serif;border: 1px solid; width:70px; text-align:center;"><b>{{$totalEbooks['royalty']}}</b></td>
+                    <td style="font-family:Verdana, Helvetica, sans-serif;border: 1px solid; width:70px; text-align:center;"><b>{{$totalEbooks['royalty']}}</b></td>
                 </tr>
             </tbody>
         </table>
@@ -150,7 +150,7 @@
     <h5 class="mt-4 my-4" ><p style="font-size: 14px; font-family:Roboto;">Should you have any questions, please don't hesitate to contact your Author Relations Officer at 800-805-0762 or send us an email at <a>info@readersmagnet.com</a></p></h5>
 
     <span style="font-size: 15px; font-family:Roboto;">Sincerely,</span>
-    <h5 style="font-size: 15px; font-family: Roboto; "><span><b style="font-size: 15px;">ReadersMagnet</b></span></h5>
+    <h5 style="font-size: 15px; font-family:Verdana, Helvetica, sans-serif; "><span><b style="font-size: 15px;">ReadersMagnet</b></span></h5>
 </div>
 
 @endsection
