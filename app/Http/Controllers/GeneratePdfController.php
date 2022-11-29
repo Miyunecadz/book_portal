077367267969f82a0358e761f580363da9417a7d
Elink mAster <arieldumalag@readersmagnet.com>
2022-11-30 05:30:38 +0800

    fix denomination

--- app/Http/Controllers/GeneratePdfController.php
+++ app/Http/Controllers/GeneratePdfController.php
@@ -203,7 +203,7 @@ class GeneratePdfController extends Controller
                     }
                 }
         
-                $totalRoyalties = number_format($totalPods['royalty'] + $totalEbooks['royalty'],2);
+               // $totalRoyalties = number_format($totalPods['royalty'] + $totalEbooks['royalty'],2);
                 $currentDate = Carbon::now()->format(' m/d/Y g:i A');
                 $imageUrl = asset('images/header.png');
           //print pdf
@@ -217,7 +217,7 @@ class GeneratePdfController extends Controller
                     'fromMonth' => $request->fromMonth,
                     'toYear' => $request->toYear,
                     'toMonth' => $request->toMonth,
-                    'allRoyal' =>$totalRoyalties,
+                    //'allRoyal' =>$totalRoyalties,
                     'currentDate' => $currentDate,
                     'imageUrl' => $imageUrl,
                 ]);
