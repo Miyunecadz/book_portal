d63a11cb1337bc9da54dee7eaf9de82d20b1ae4d
Elink mAster <arieldumalag@readersmagnet.com>
2022-12-03 00:54:13 +0800

    remove total ebook pricd

--- resources/views/prev.blade.php
+++ resources/views/prev.blade.php
@@ -129,7 +129,7 @@
                         <td colspan="3" style="border: 1px solid; width:90px; "><input hidden type="text" name="book[]" multiple="multiple" id="book" value="{{$ebook['books']}}" class="form-select select2"><b>{{$ebook['title']}}</b></td>
                         <td style="border: 1px solid; width:70px; text-align:center;"><b></b></td>
                         <td style="border: 1px solid; width:70px; text-align:center;">{{$ebook['quantity']}}</td>
-                        <td style="border: 1px solid; width:70px; text-align:center;">${{number_format($ebook['price'],2)}}</td>
+                        <td style="border: 1px solid; width:70px; text-align:center;"></td>
 
                         <td style="border: 1px solid; width:70px; text-align:center;">${{$ebook['royalty']}}</td>
                     </tr>
@@ -152,7 +152,7 @@
                     <td colspan="3" style="border: 1px solid; width:90px; "><b>${{$totalEbooks['title']}}</b></td>
                     <td style="border: 1px solid; width:70px; text-align:center;"><b></b></td>
                     <td style="border: 1px solid; width:70px; text-align:center;"><b>{{$totalEbooks['quantity']}}</b></td>
-                    <td style="border: 1px solid; width:70px; text-align:center;"><b>${{number_format($totalEbooks['price'],2)}}</b></td>
+                    <td style="border: 1px solid; width:70px; text-align:center;"><b></b></td>
                  
                     <td style="border: 1px solid; width:70px; text-align:center;"><b>${{number_format($totalEbooks['royalty'],2)}}</b></td>
                 </tr>
