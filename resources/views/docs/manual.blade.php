b944e84629f4f389806df968f91a53979162e1b4
Elink mAster <arieldumalag@readersmagnet.com>
2022-12-13 21:59:18 +0800

    tweak to show inst manual

--- resources/views/docs/manual.blade.php
+++ resources/views/docs/manual.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="container ">
-<iframe src="{{asset('instmanual.pdf')}}" width="1250" height="1000" alt="pdf" >
+<iframe src="{{asset('images/instmanual.pdf')}}" width="1250" height="1000" alt="pdf" >
 </iframe>
 
 </div>
