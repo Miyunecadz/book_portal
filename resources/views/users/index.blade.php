b227852ffc296e465f665e911d530b2e2049f86e
Elink mAster <arieldumalag@readersmagnet.com>
2023-01-06 06:00:47 +0800

    remove user import since yet to function

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -15,7 +15,7 @@
                    
                 </div>
                 <div class="ms-auto">
-                <a href="{{ route('pod.import-page') }}" class="btn btn-outline-primary btn-sm">
+                {{-- <a href="{{ route('usrinfo.import-page') }}" class="btn btn-outline-primary btn-sm">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                             class="bi bi-download" >
                             <path
@@ -24,7 +24,8 @@
                                 d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z" />
                         </svg>
                         Bulk Import
-                    </a>
+                    </a>--}}
+               
                     @if( auth()->user()->usertype() == 1 &&   auth()->user()->usertype() == 2 )
                     <a href="{{ route('pod.clear') }}"
                 onclick="return confirm('Are you sure you want to Clear ALL?')"
