18a4320800b2b9756c29a491a744c7eb6c5a0ed1
Elink mAster <arieldumalag@readersmagnet.com>
2023-01-11 06:03:21 +0800

    re tweak for user access perimission

--- resources/views/rejecteds/ebooks/index.blade.php
+++ resources/views/rejecteds/ebooks/index.blade.php
@@ -51,7 +51,7 @@
                             <th>Retail Price</th>
                             <th>Proceeds of Sale Due Publisher</th>
                             <th>Author Royalty</th>
-                            @if( auth()->user()->usertype() == 1  ||  auth()->user()->usertype() == 2 )
+                            @if( auth()->user()->usertype() == 1  ||  auth()->user()->usertype() == 2 ||  auth()->user()->usertype() == 3 )
                             <th>Action</th>
                             @endif
                         </tr>
@@ -67,10 +67,12 @@
                                 <td>${{ $rejected_ebook->price }}</td>
                                 <td>{{ $rejected_ebook->proceeds }}</td>
                                 <td>${{ $rejected_ebook->royalty }}</td>
-                                @if( auth()->user()->usertype() == 1  ||  auth()->user()->usertype() == 2 )
+                               
                                 <td>
+                                
                                     <div class="d-flex gap-2 justify-content-center">
-                                        <a href="{{ route('rejecteds-ebooks.edit', ['rejected_ebook' => $rejected_ebook]) }}"
+                                    @if( auth()->user()->usertype() == 1  ||  auth()->user()->usertype() == 2 || auth()->user()->usertype() == 3 ) 
+                                    <a href="{{ route('rejecteds-ebooks.edit', ['rejected_ebook' => $rejected_ebook]) }}"
                                             class="btn btn-outline-warning">
                                             <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15"
                                                 fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
@@ -78,6 +80,8 @@
                                                     d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z" />
                                             </svg>
                                         </a>
+                                        @endif
+                                        @if( auth()->user()->usertype() == 1  ||  auth()->user()->usertype() == 2 )
                                         <a href="{{ route('rejecteds-ebooks.delete', ['rejected_ebook' => $rejected_ebook]) }}"
                                             onclick="return confirm('Are you sure you want to delete this file?')"
                                             class="btn btn-outline-danger">
@@ -89,9 +93,10 @@
                                                     d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
                                             </svg>
                                         </a>
+                                        @endif
                                     </div>
                                 </td>
-                                @endif
+                               
                             </tr>
                         @empty
                             <tr>
