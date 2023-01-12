f1bd4003f83fe69ecba159711560b0465e268133
Elink mAster <arieldumalag@readersmagnet.com>
2023-01-13 03:14:55 +0800

    Adjust access privileges

--- resources/views/author/index.blade.php
+++ resources/views/author/index.blade.php
@@ -33,7 +33,7 @@
                       </svg>
                     Bulk Import
                 </a>
-                @endif
+            
                 <a href="{{route('author.create')}}" class="btn btn-outline-success">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-plus" viewBox="0 0 16 16">
                         <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H1s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C9.516 10.68 8.289 10 6 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
@@ -41,6 +41,7 @@
                       </svg>
                     Add Author
                 </a>
+                @endif
                 @if( auth()->user()->usertype() == 1 )
                 <a href="{{ route('author.clear') }}"
                 onclick="return confirm('Are you sure you want to Clear ALL?')"
@@ -62,6 +63,7 @@
                         <th>Contact Number</th>
                         <th>Address</th>
                         <th>Special Royalty</th>
+                        <th>Assign Pubcon</th>
                         @if( auth()->user()->usertype() == 1 || auth()->user()->usertype() == 2 || auth()->user()->usertype() == 3 )
                         <th>Action</th>
                         @endif
@@ -79,6 +81,11 @@
                         <td>{{$author->contact_number}}</td>
                         <td>{{$author->address}}</td>
                         <td>{{$author->specroyal}}</td>
+                        <td>
+                        @if(!empty($author->user_id))
+                            {{$author->user->firstname}}, {{$author->user->lastname}}
+                        @endif
+                        </td>
                         @if( auth()->user()->usertype() == 1 || auth()->user()->usertype()== 2 || auth()->user()->usertype()== 3)
                         <td>
                         @if( auth()->user()->usertype() == 1 || auth()->user()->usertype()== 2 || auth()->user()->usertype() == 3)
